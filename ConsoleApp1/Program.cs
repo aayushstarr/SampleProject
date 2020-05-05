0d38816fa83efd54f38f788ed2681614f0f4c96f
Ayush Singh Tomar <AF5E@eurofinsbpt.com>
2020-05-06 00:47:20 +0530

    revert

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,6 @@ namespace ConsoleApp1 {
   class Program {
     static void Main(string[] args) {
       Console.WriteLine("Hello World!");
-      Console.WriteLine("added r1");
-      Console.WriteLine("Added for r2");
-      Console.WriteLine("Added another from r2");
     }
   }
 }
